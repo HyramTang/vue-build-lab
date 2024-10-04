d65807b8e18aeb5f4b239f196a4887cd410d9d16
Hyram Tang <hyram.tang@gmail.com>
2024-10-04 23:27:30 +0800

    feat: split-by-experience

--- vue.config.js
+++ vue.config.js
@@ -9,19 +9,24 @@ module.exports = {
         maxInitialRequests: 6, // 每个入口和它的同步依赖最多能被拆分的数量，同一 domain 最大 6 个
         enforceSizeThreshold: 50000, // 强制执行拆分的体积阈值并忽略其他限制
         cacheGroups: {
-          // 对 node_modules 中的每个依赖单独打包
-          defaultVendors: false, // 禁用默认 vendors 规则
-          vendors: {
+          libs: {
+            // 第三方库：Vue、VueRouter、Vuex、lodash
+            name: "chunk-libs",
             test: /[\\/]node_modules[\\/]/,
-            name(module) {
-              // 使用模块名称进行拆分，比如: node_modules_lodash_chunk.js
-              const packageName = module.context.match(
-                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-              )[1];
-              return `npm.${packageName.replace("@", "")}`;
-            },
-            chunks: "all",
             priority: 10,
+            chunks: "initial", // 只打包初始时依赖的第三方
+          },
+          "bootstrap-vue": {
+            // bootstrap-vue 单独拆包
+            name: "chunk-bootstrap-vue",
+            test: /[\\/]node_modules[\\/]bootstrap-vue[\\/]/,
+            priority: 20, // 权重要大于 libs
+          },
+          commons: {
+            // 公共模块包
+            name: `chunk-commons`,
+            minChunks: 2,
+            priority: 0,
             reuseExistingChunk: true,
           },
         },
